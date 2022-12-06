624973d4ff3c7b779018a017010a9b8ccc87e45d
Frank Moreno <Frank.Moreno95@gmail.com>
2022-12-06 11:03:36 -0600

    move around

--- day6/day6.go
+++ day6/day6.go
@@ -1,6 +1,6 @@
 package day6
 
-const LENGTH = 13
+const LENGTH = 14
 
 func Part1(input string) int {
 	left, right := 0, 0
@@ -8,16 +8,16 @@ func Part1(input string) int {
 
 	for ; right < len(input); right++ {
 		foundIndex, ok := found[input[right]]
-		if !ok {
-			if right-left == LENGTH {
-				return right + 1
-			}
-		} else {
+
+		if ok {
 			for i := left; i <= foundIndex; i++ {
 				delete(found, input[i])
 			}
 			left = foundIndex + 1
+		} else if right-left+1 == LENGTH {
+			return right + 1
 		}
+
 		found[input[right]] = right
 	}
 
